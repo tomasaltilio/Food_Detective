f5e8ab0e08daa7bdfa0a2eed1cb760ec79f56011
Tomas Altilio <tomas.altilio@gmail.com>
2021-03-11 16:16:55 -0300

    Changed heroku email

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"tomas.altilio@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableCORS = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
