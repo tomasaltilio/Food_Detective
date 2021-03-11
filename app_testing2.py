8e3d9c1a10e2c985e5a0af465d1be12880e6bb2c
Tomas Altilio <tomas.altilio@gmail.com>
2021-03-11 16:27:00 -0300

    Updated requirements

--- app_testing2.py
+++ app_testing2.py
@@ -8,7 +8,6 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.resnet import preprocess_input
 import time
-import sys
 
 
 categories = [b'apple_pie',
